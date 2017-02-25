3570de954a8ca1b827e33d448889812c34e34bd4
AvneetKhaira <avneet.khaira7@gmail.com>
2017-02-25 09:40:06 -0500

    added the edit book functionality

--- server/routes/books.js
+++ server/routes/books.js
@@ -57,17 +57,51 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    try {
+      // get a reference to the id from the url
+      let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+        // find one book by its id
+      book.findById(id, (err, books) => {
+        if(err) {
+          console.log(err);
+          res.end(error);
+        } else {
+          // show the book details view
+          res.render('books/details', {
+              title: 'Book Details',
+              books: books
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.redirect('/errors/404');
+    }
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
+    // get a reference to the id from the url
+        let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+        let updatedBook = book({
+          "_id": id,
+          "Title": req.body.title,
+          "Price": req.body.price,
+          "Author": req.body.author,
+          "Genre": req.body.genre
+        });
+
+        book.update({_id: id}, updatedBook, (err) => {
+          if(err) {
+            console.log(err);
+            res.end(err);
+          } else {
+            // refresh the book List
+            res.redirect('/books');
+          }
+        });
 
 });
 
